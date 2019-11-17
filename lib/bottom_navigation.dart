d5ca19fe576c8b27d4c3071911e30fa1d2e9a896
Aengus <sunyongsheng6511@gmail.com>
2019-11-17 20:48:34 +0800

    增加底部导航栏

---
+++ lib/bottom_navigation.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import 'package:allpass/pages/password_page.dart';
+import 'package:allpass/pages/card_page.dart';
+import 'package:allpass/pages/setting_page.dart';
+
+class BottomNavigationWidget extends StatefulWidget {
+
+  @override
+  _BottomNavigationWidget createState() => _BottomNavigationWidget();
+}
+
+class _BottomNavigationWidget extends State<BottomNavigationWidget> {
+
+  final Color _appColor = Colors.blue;
+  List<Widget> pagesList = List();
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    pagesList..add(PasswordPage())
+             ..add(CardPage())
+             ..add(SettingPage());
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pagesList[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.supervised_user_circle,
+              color: _appColor,
+            ),
+            title: Text("密码")
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.credit_card,
+                color: _appColor,
+              ),
+              title: Text("卡片")
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.settings,
+                color: _appColor,
+              ),
+              title: Text("设置")
+          ),
+        ],
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
