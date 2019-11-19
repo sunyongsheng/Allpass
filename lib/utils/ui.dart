31becdd4cc0f7f5222e7f013b3b823ee44ec7ae0
Aengus Sun <sunyongsheng6511@gmail.com>
2019-11-19 16:52:07 +0800

    新增UI类，保存软件各颜色与Style格式

---
+++ lib/utils/ui.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TextUI {
+
+  TextUI._();
+
+  static final TextStyle mainTitleStyle = TextStyle(fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold);
+  static final TextStyle firstTitleStyleBlue = TextStyle(fontSize: 18, color: Colors.blue);
+  static final TextStyle secondTitleStyleBlue = TextStyle(fontSize: 16, color: Colors.blue);
+
+  static final TextStyle firstTitleStyleBlack = TextStyle(fontSize: 18, color: Colors.black);
+  static final TextStyle secondTitleStyleBlack = TextStyle(fontSize: 16, color: Colors.black);
+
+}
+
+class ColorUI {
+
+  static final Color mainColor = Colors.blue;
+  static final Color mainBackgroundColor = Colors.white;
+
+}
