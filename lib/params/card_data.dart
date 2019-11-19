e7a9eaeafbac1efb22c7439f2576844b77229281
Aengus Sun <sunyongsheng6511@gmail.com>
2019-11-19 23:54:51 +0800

    保存用户密码、卡片相关数据

---
+++ lib/params/card_data.dart
@@ -0,0 +1,9 @@
+import 'package:allpass/bean/card_bean.dart';
+
+/// 用于存储所有“卡片”数据
+/// @author Aengus Sun
+class CardData {
+
+  static List<CardBean> cardData = List();
+
+}
---
+++ lib/params/password_data.dart
@@ -0,0 +1,11 @@
+import 'package:allpass/bean/password_bean.dart';
+
+/// 用于存储所有“密码”数据
+/// @author Aengus Sun
+class PasswordData {
+
+  static List<PasswordBean> passwordData = List();
+
+
+
+}
