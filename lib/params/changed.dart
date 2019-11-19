c20135c2d354dbee109ea68d2aeef716257eaaa0
Aengus Sun <sunyongsheng6511@gmail.com>
2019-11-19 22:54:43 +0800

    重命名类，增加labelList

--- lib/params/changed.dart
+++ lib/params/changed.dart
@@ -1,8 +1,8 @@
-class FolderList {
+class FolderAndLabelList {
 
-  FolderList._();
+  FolderAndLabelList._();
 
   static List<String> folderList = List.of(<String>['默认', '论坛', '社交', '游戏', '其他']);
-
+  static List<String> labelList = List.of(<String>["默认", "论坛", "学习", "娱乐", "游戏"]);
 
 }
