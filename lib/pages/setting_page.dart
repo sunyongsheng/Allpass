7ee7235ae601b4e4711501dffe9c73f9c8501984
Aengus Sun <sunyongsheng6511@gmail.com>
2019-11-20 23:55:18 +0800

    测试页面

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -1,3 +1,4 @@
+import 'package:allpass/params/changed.dart';
 import 'package:flutter/material.dart';
 
 import 'package:allpass/utils/allpass_ui.dart';
@@ -14,17 +15,52 @@ class _SettingPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<_SettingPage> {
+  var _value = "默认";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return WillPopScope(
+        onWillPop: () {
+      Navigator.of(context).pop();
+      return Future<bool>.value(false);
+    },
+    child: Scaffold(
       appBar: AppBar(
         title: Text("设置", style: AllpassTextUI.mainTitleStyle,),
         centerTitle: true,
         backgroundColor: AllpassColorUI.mainBackgroundColor,
         elevation: 0,
       ),
-      body: Text("设置"),
+      body: SingleChildScrollView(child:Column(
+        children: <Widget>[
+          Center(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                DropdownButton(
+                  value: _value,
+                  onChanged: (newValue) {
+                    setState(() {
+                      _value = newValue;
+                    });
+                  },
+                  items: FolderAndLabelList.folderList
+                      .map<DropdownMenuItem<String>>((value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  style: AllpassTextUI.firstTitleStyleBlack,
+                  elevation: 8,
+                  iconSize: 30,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),),
       backgroundColor: AllpassColorUI.mainBackgroundColor,
+    )
     );
   }
 }
