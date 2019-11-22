a50d986f5c40ae20bfd6708a0460ce5e37f2a261
Aengus Sun <sunyongsheng6511@gmail.com>
2019-11-22 22:52:27 +0800

    添加注释

--- lib/params/card_data.dart
+++ lib/params/card_data.dart
@@ -3,9 +3,9 @@ import 'package:allpass/bean/card_bean.dart';
 /// 用于存储所有“卡片”数据
 /// @author Aengus Sun
 class CardData {
-
+  // 保存所有卡片数据
   static List<CardBase> cardData = List();
-
+  // 保存卡片唯一Key
   static Set<int> cardKeySet = Set();
 
 }
