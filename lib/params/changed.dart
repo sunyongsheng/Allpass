6b98f2600f2e2b4597422068e95acad779608318
Aengus Sun <sunyongsheng6511@gmail.com>
2019-11-23 11:32:40 +0800

    添加label：社交

--- lib/params/changed.dart
+++ lib/params/changed.dart
@@ -4,6 +4,6 @@ class FolderAndLabelList {
   FolderAndLabelList._();
 
   static List<String> folderList = List.of(<String>['默认', '论坛', '社交', '游戏', '其他']);
-  static List<String> labelList = List.of(<String>["默认", "论坛", "学习", "娱乐", "游戏"]);
+  static List<String> labelList = List.of(<String>["默认", "论坛", "学习", "娱乐", "游戏", "社交"]);
 
 }
