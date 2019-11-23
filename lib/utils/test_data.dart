1ed2d7f15d612f8b9085704729a8ec629f9f9b4d
Aengus Sun <sunyongsheng6511@gmail.com>
2019-11-23 11:37:02 +0800

    格式化代码

--- lib/utils/test_data.dart
+++ lib/utils/test_data.dart
@@ -3,17 +3,17 @@ import 'package:allpass/bean/card_bean.dart';
 
 /// 密码测试数据
 class PasswordTestData {
-
   static List<PasswordBean> passwordList = List();
 
   PasswordTestData() {
     if (passwordList.length == 0) {
-      passwordList.add(
-          PasswordBean("sys6511@126.com", "1234", "https://www.weibo.com", label: List()..addAll(["默认","论坛", "社交"])));
+      passwordList.add(PasswordBean(
+          "sys6511@126.com", "1234", "https://www.weibo.com",
+          label: List()..addAll(["默认", "论坛", "社交"])));
       passwordList.add(
           PasswordBean("sys6511@126.com", "12345", "https://www.zhihu.com"));
-      passwordList.add(PasswordBean(
-          "sys6511@126.com", "31238912", "https://www.126.com"));
+      passwordList.add(
+          PasswordBean("sys6511@126.com", "31238912", "https://www.126.com"));
       passwordList.add(PasswordBean(
           "sunyongsheng6511@gmail.com", "joi123123", "https://www.gmail.com"));
       passwordList.add(
@@ -21,7 +21,7 @@ class PasswordTestData {
       passwordList.add(
           PasswordBean("sys6511@126.com", "1234", "https://www.weibo.com"));
       passwordList.add(
-          PasswordBean( "sys6511@126.com", "1234", "https://www.weibo.com"));
+          PasswordBean("sys6511@126.com", "1234", "https://www.weibo.com"));
       passwordList.add(
           PasswordBean("sys6511@126.com", "1234", "https://www.weibo.com"));
       passwordList.add(
@@ -29,22 +29,19 @@ class PasswordTestData {
       passwordList.add(
           PasswordBean("sys6511@126.com", "1234", "https://www.weibo.com"));
     }
-
   }
-
 }
 
 /// 卡片测试数据
 class CardTestData {
-
   static List<CardBean> cardList = List();
 
   CardTestData() {
     if (cardList.length == 0) {
-      cardList..add(
-          CardBean("Aengus", "1233455214123123", name: "Aengus的身份证"))..add(
-          CardBean("Joye", "12312312312312312", name: "Joyed的身份证"))..add(
-          CardBean("Jike", "123146452132446748", name: "工商银行"));
+      cardList
+        ..add(CardBean("Aengus", "1233455214123123", name: "Aengus的身份证"))
+        ..add(CardBean("Joye", "12312312312312312", name: "Joyed的身份证"))
+        ..add(CardBean("Jike", "123146452132446748", name: "工商银行"));
     }
   }
 }
