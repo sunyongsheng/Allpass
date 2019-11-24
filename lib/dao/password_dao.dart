11ac0d4f7901dd8ad6d036afb1bc427d5b8bd4d5
Aengus Sun <sunyongsheng6511@gmail.com>
2019-11-24 20:15:46 +0800

    新增PasswordDao

---
+++ lib/dao/password_dao.dart
@@ -0,0 +1,77 @@
+import 'package:sqflite/sqflite.dart';
+
+import 'package:allpass/bean/password_bean.dart';
+import 'package:allpass/utils/db_provider.dart';
+
+class PasswordDao extends BaseDBProvider {
+
+  // 表名
+  final String name = "allpass_password";
+
+  // 表主键字段
+  final String columnId = "uniqueKey";
+
+  @override
+  tableName() {
+    return name;
+  }
+
+  // 创建表的sql
+  @override
+  tableSqlString() {
+    return tableBaseString(name, columnId) +
+      '''
+      name TEXT NOT NULL,
+      username TEXT NOT NULL,
+      password TEXT NOT NULL,
+      url TEXT NOT NULL,
+      forder TEXT DEFALUT '默认',
+      notes TEXT,
+      label TEXT,
+      fav INTEGER DEFAULT 0)
+      ''';
+  }
+
+  // 插入记录
+  Future insert(PasswordBean bean) async {
+    Database db = await getDataBase();
+    return db.insert(name, toMap(bean));
+  }
+
+  // 根据uniqueKey查询记录
+  Future<PasswordBean> getPasswordBean(String id) async {
+    Database db = await getDataBase();
+    List<Map<String, dynamic>> maps = await db.query(name);
+    if (maps.length > 0) {
+      return PasswordBean.fromJson(maps.first);
+    }
+    return null;
+  }
+
+  // 获取密码
+  Future<List<PasswordBean>> getPasswordBeanList() async {
+    Database db = await getDataBase();
+    List<Map<String, dynamic>> maps = await db.query(name);
+    if (maps.length > 0) {
+      List<PasswordBean> res = maps.map((item) => PasswordBean.fromJson(item)).toList();
+      return res;
+    }
+    return null;
+  }
+
+  Map<String, dynamic> toMap(PasswordBean bean) {
+    Map<String, dynamic> map = {
+      "uniqueKey": bean.uniqueKey,
+      "name": bean.name,
+      "username": bean.username,
+      "password": bean.password,
+      "url": bean.url,
+      "folder": bean.folder,
+      "fav": bean.fav,
+      "notes": bean.notes,
+      "label": bean.label
+    };
+    return map;
+  }
+
+}
