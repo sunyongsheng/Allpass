06b41f2693f59adc551694f71bf48dd4a3941739
Aengus Sun <sunyongsheng6511@gmail.com>
2019-11-24 21:07:02 +0800

    修改部分方法名

--- lib/dao/password_dao.dart
+++ lib/dao/password_dao.dart
@@ -39,7 +39,7 @@ class PasswordDao extends BaseDBProvider {
   }
 
   // 根据uniqueKey查询记录
-  Future<PasswordBean> getPasswordBean(String id) async {
+  Future<PasswordBean> getPasswordBeanById(String id) async {
     Database db = await getDataBase();
     List<Map<String, dynamic>> maps = await db.query(name);
     if (maps.length > 0) {
@@ -49,7 +49,7 @@ class PasswordDao extends BaseDBProvider {
   }
 
   // 获取所有的密码List
-  Future<List<PasswordBean>> getPasswordBeanList() async {
+  Future<List<PasswordBean>> getAllPasswordBeanList() async {
     Database db = await getDataBase();
     List<Map<String, dynamic>> maps = await db.query(name);
     if (maps.length > 0) {
