edaa770f056e4338a3d75c7278821f7b457447da
Aengus Sun <sunyongsheng6511@gmail.com>
2019-11-25 21:15:41 +0800

    完成搜索功能

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -4,11 +4,11 @@ import 'package:flutter/services.dart';
 import 'package:allpass/bean/password_bean.dart';
 import 'package:allpass/bean/card_bean.dart';
 import 'package:allpass/params/allpass_type.dart';
-import 'package:allpass/params/card_data.dart';
-import 'package:allpass/params/password_data.dart';
 import 'package:allpass/utils/allpass_ui.dart';
 import 'package:allpass/pages/view_and_edit_card_page.dart';
 import 'package:allpass/pages/view_and_edit_password_page.dart';
+import 'package:allpass/dao/card_dao.dart';
+import 'package:allpass/dao/password_dao.dart';
 
 class SearchPage extends StatefulWidget {
   final AllpassType type;
@@ -24,6 +24,11 @@ class _SearchPage extends State<SearchPage> {
 
   String _searchText = "";
   var _searchController;
+  List<dynamic> _searchList = List();
+  List<Widget> _result = List();
+
+  PasswordDao passwordDao = PasswordDao();
+  CardDao cardDao = CardDao();
 
   int _currentKey = -1;
 
@@ -42,19 +47,41 @@ class _SearchPage extends State<SearchPage> {
         elevation: 0,
         toolbarOpacity: 1,
       ),
-      body: ListView(
-        children: getSearchResult(),
+      body: FutureBuilder(
+        future: getSearchResult(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+              return Center(
+                child: Text("搜索中..."),
+              );
+            case ConnectionState.done:
+              return _result.length == 0
+                  ? Center(
+                      child: Text("无结果"),
+                    )
+                  : ListView(
+                      children: _result,
+                    );
+            default:
+              return Center(child: Text("未知状态，请联系开发者：sys6511@126.com"));
+          }
+        },
       ),
     );
   }
 
-  List<Widget> getSearchResult() {
-    List<Widget> res = List();
+  Future<Null> getSearchResult() async {
+    _result.clear();
     if (_type == AllpassType.PASSWORD) {
-      for (var item in PasswordData.passwordData) {
+      _searchList = await passwordDao.getAllPasswordBeanList();
+      for (var item in _searchList) {
         if (item.name.contains(_searchText) ||
-            item.username.contains(_searchText)) {
-          res.add(ListTile(
+            item.username.contains(_searchText) ||
+            item.notes.contains(_searchText)) {
+          _result.add(ListTile(
               title: Text(item.name),
               subtitle: Text(item.username),
               onTap: () {
@@ -69,10 +96,12 @@ class _SearchPage extends State<SearchPage> {
         }
       }
     } else {
-      for (var item in CardData.cardData) {
+      _searchList = await cardDao.getAllCardBeanList();
+      for (var item in _searchList) {
         if (item.name.contains(_searchText) ||
-            item.ownerName.contains(_searchText)) {
-          res.add(ListTile(
+            item.ownerName.contains(_searchText) ||
+            item.notes.contains(_searchText)) {
+          _result.add(ListTile(
             title: Text(item.name),
             subtitle: Text(item.ownerName),
             onTap: () {
@@ -88,7 +117,6 @@ class _SearchPage extends State<SearchPage> {
         }
       }
     }
-    return res;
   }
 
   /// 搜索栏
@@ -124,7 +152,7 @@ class _SearchPage extends State<SearchPage> {
             ),
             FlatButton(
               onPressed: () {
-                Navigator.pop(context);
+                Navigator.pop(context, null);
               },
               child: Text("取消", style: AllpassTextUI.secondTitleStyleBlack),
               splashColor: AllpassColorUI.mainBackgroundColor,
@@ -144,11 +172,13 @@ class _SearchPage extends State<SearchPage> {
           title: Text("查看"),
           onTap: () {
             Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => ViewAndEditPasswordPage(
-                        data, "查看密码", true))).then((reData) =>
-                this.setState(() => updatePasswordBean(reData, _currentKey)));
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            ViewAndEditPasswordPage(data, "查看密码", true)))
+                .then((reData) {
+              if (reData != null) passwordDao.updatePasswordBean(reData);
+            });
           },
         ),
         ListTile(
@@ -156,11 +186,13 @@ class _SearchPage extends State<SearchPage> {
           title: Text("编辑"),
           onTap: () {
             Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => ViewAndEditPasswordPage(
-                        data, "编辑密码", false))).then((reData) =>
-                this.setState(() => updatePasswordBean(reData, _currentKey)));
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            ViewAndEditPasswordPage(data, "编辑密码", false)))
+                .then((reData) {
+              if (reData != null) passwordDao.updatePasswordBean(reData);
+            });
           },
         ),
         ListTile(
@@ -182,7 +214,9 @@ class _SearchPage extends State<SearchPage> {
         ListTile(
           leading: Icon(Icons.delete_outline),
           title: Text("删除密码"),
-          onTap: () => setState(() => deletePasswordBean(_currentKey)),
+          onTap: () {
+            passwordDao.deletePasswordBeanById(_currentKey);
+          },
         )
       ],
     );
@@ -198,25 +232,27 @@ class _SearchPage extends State<SearchPage> {
             title: Text("查看"),
             onTap: () {
               Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => ViewAndEditCardPage(
-                          cardBean, "查看卡片", true))).then((resData) =>
-                  this.setState(() => updateCardBean(resData, _currentKey)));
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              ViewAndEditCardPage(cardBean, "查看卡片", true)))
+                  .then((resData) {
+                if (resData != null) cardDao.updatePasswordBean(resData);
+              });
             }),
         ListTile(
-          leading: Icon(Icons.edit),
-          title: Text("编辑"),
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        ViewAndEditCardPage(cardBean, "编辑卡片", false))).then(
-                (resData) =>
-                    this.setState(() => updateCardBean(resData, _currentKey)));
-          },
-        ),
+            leading: Icon(Icons.edit),
+            title: Text("编辑"),
+            onTap: () {
+              Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              ViewAndEditCardPage(cardBean, "编辑卡片", false)))
+                  .then((resData) {
+                if (resData != null) cardDao.updatePasswordBean(resData);
+              });
+            }),
         ListTile(
           leading: Icon(Icons.person),
           title: Text("复制用户名"),
@@ -234,7 +270,7 @@ class _SearchPage extends State<SearchPage> {
         ListTile(
           leading: Icon(Icons.delete_outline),
           title: Text("删除卡片"),
-          onTap: () => setState(() => deleteCardBean(_currentKey)),
+          onTap: () => cardDao.deleteCardBeanById(_currentKey),
         )
       ],
     );
