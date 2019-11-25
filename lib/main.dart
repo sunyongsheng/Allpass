74c7799b5258c98373027f058e4a2d5b45ff87a4
Aengus Sun <sunyongsheng6511@gmail.com>
2019-11-25 22:33:54 +0800

    添加参数初始化

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 import 'package:allpass/bottom_navigation.dart';
+import 'package:allpass/params/params.dart';
 
 void main() {
+  Params.paramsInit();
   runApp(Allpass());
 }
 
 class Allpass extends StatelessWidget {
 
-  // TODO 标签中不允许 ~ 符号
+  // TODO 标签中不允许 ~ 符号，文件夹不允许英文逗号
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
