7d4223d7b02832b4f2115f4649670fa6926a914a
Aengus Sun <sunyongsheng6511@gmail.com>
2019-11-26 11:07:43 +0800

    新增添加属性对话框，用于弹出新增标签或文件夹对话框

---
+++ lib/widgets/add_category_dialog.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+import 'package:allpass/params/params.dart';
+import 'package:allpass/utils/allpass_ui.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+/// 添加属性对话框
+class AddCategoryDialog extends StatefulWidget {
+
+  final String categoryName;
+
+  AddCategoryDialog(this.categoryName);
+
+  @override
+  _AddLabelDialog createState() {
+    return _AddLabelDialog(categoryName);
+  }
+
+}
+
+class _AddLabelDialog extends State<AddCategoryDialog> {
+
+  var _addTextController = TextEditingController();
+  bool _inputFormatCorr = true;
+
+  final String categoryName;
+
+  _AddLabelDialog(this.categoryName);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("新建$categoryName"),
+      shape: RoundedRectangleBorder(
+        borderRadius:
+        BorderRadius.all(Radius.circular(15),),
+      ),
+      content: Theme(
+        data: ThemeData(primaryColor: AllpassColorUI.mainColor),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextField(
+              autofocus: true,
+              decoration: InputDecoration(
+                  errorText: _inputFormatCorr?"":"$categoryName名不允许包含“,”或“~”",
+              ),
+              controller: _addTextController,
+              onChanged: (text) {
+                if (text.contains(",") || text.contains("~")) {
+                  setState(() {
+                    _inputFormatCorr = false;
+                  });
+                } else {
+                  _addTextController.text = text;
+                }
+              }
+            ),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+            _addTextController.text = "";
+          },
+          child: Text('取消'),
+          textColor: AllpassColorUI.mainColor,
+        ),
+        FlatButton(
+          onPressed: () {
+            if (_inputFormatCorr && _addTextController.text != "") {
+              if (categoryName == "标签") {
+                Params.labelList.add(_addTextController.text);
+                Params.labelParamsPersistence();
+              } else {
+                Params.folderList.add(_addTextController.text);
+                Params.folderParamsPersistence();
+              }
+              Fluttertoast.showToast(msg: "添加$categoryName ${_addTextController.text}");
+            }
+          },
+          child: Text('提交'),
+          textColor: AllpassColorUI.mainColor,
+        ),
+      ],
+    );
+  }
+
+}
