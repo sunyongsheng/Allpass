ffb07e3c24a5cbde7804c8cbe714017fc15804b8
Aengus Sun <sunyongsheng6511@gmail.com>
2019-11-28 16:41:05 +0800

    增加从CSV文件导入Bean的函数，返回List<Bean>

--- lib/utils/csv_helper.dart
+++ lib/utils/csv_helper.dart
@@ -47,4 +47,71 @@ class CsvHelper {
     return null;
   }
 
+  /// 从csv文件中导入Password，返回List<PasswordBean>
+  Future<List<PasswordBean>> passwordImportFromCsv(String path) async {
+    List<PasswordBean> res = List();
+    File file = File(path);
+    if (!file.existsSync()) throw UnsupportedError("文件不存在!");
+    String fileContext = file.readAsStringSync();
+    List<String> text = fileContext.split("\n");
+    if (text.length <= 1) return null;
+    else {
+      assert(text[0] == "name,username,password,url,folder,notes,label,fav", "文件格式不正确！");
+      // 对接下来的每一行
+      for (var item in text.sublist(1)) {
+        List<String> attribute = item.split(",");
+        if (attribute == null) continue;
+        List<String> label = List();
+        for (String la in attribute[6].split("~")) {
+          if (la != "" && la != " " && la != "~" && la != ",") label.add(la);
+        }
+        res.add(PasswordBean(
+          name: attribute[0] == null ? "" : attribute[0],
+          username: attribute[1] == null ? "" : attribute[1],
+          password: attribute[2] == null ? "" : attribute[2],
+          url: attribute[3] == null ? "" : attribute[3],
+          folder: attribute[4] == null ? "默认" : attribute[4],
+          notes: attribute[5] == null ? "" : attribute[5],
+          label: label,
+          fav: int.parse(attribute[7]) == null ? "" : int.parse(attribute[7]),
+        ));
+      }
+    }
+    return res;
+  }
+
+  /// 从csv文件中导入Card
+  Future<List<CardBean>> cardImportFromCsv(String path) async {
+    List<CardBean> res = List();
+    File file = File(path);
+    if (!file.existsSync()) throw UnsupportedError("文件不存在!");
+    String fileContext = file.readAsStringSync();
+    List<String> text = fileContext.split("\n");
+    if (text.length <= 1) return null;
+    else {
+      assert(text[0] == "name,ownerName,cardId,url,telephone,folder,notes,label,fav", "文件格式不正确！");
+      // 对接下来的每一行
+      for (var item in text.sublist(1)) {
+        List<String> attribute = item.split(",");
+        if (attribute == null) continue;
+        List<String> label = List();
+        for (String la in attribute[7].split("~")) {
+          if (la != "" && la != " " && la != "~" && la != ",") label.add(la);
+        }
+        res.add(CardBean(
+          name: attribute[0] == null ? "" : attribute[0],
+          ownerName: attribute[1] == null ? "" : attribute[1],
+          cardId: attribute[2] == null ? "" : attribute[2],
+          url: attribute[3] == null ? "" : attribute[3],
+          telephone: attribute[4] == null ? "" : attribute[4],
+          folder: attribute[5] == null ? "" : attribute[5],
+          notes: attribute[6] == null ? "" : attribute[6],
+          label: label,
+          fav: int.parse(attribute[8]) == null ? 0 : int.parse(attribute[8]),
+        ));
+      }
+    }
+    return res;
+  }
+
 }
