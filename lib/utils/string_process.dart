a1c415c6e1bc99a9e0d05f5712979e884d87625e
Aengus Sun <sunyongsheng6511@gmail.com>
2019-11-29 13:24:24 +0800

    新增字符串处理文件；新增以~为分隔符的list2Str(),str2List()

---
+++ lib/utils/string_process.dart
@@ -0,0 +1,23 @@
+/// 字符串处理
+
+/// 将List转化为以[~]为分隔符的字符串
+String list2Str(List<String> list) {
+  String res = "";
+  for (var item in list) {
+    res += item;
+    if (item != list.last) res += "~";
+  }
+  return res;
+}
+
+/// 将以[~]为分隔符的字符串转化为List
+List<String> str2List(String string) {
+  List<String> res = List();
+  List<String> list = string.split("~");
+  if (list != null) {
+    for (var item in list) {
+      if (item != "" && item != "," && item != " " && item != "~") res.add(item);
+    }
+  }
+  return res;
+}
