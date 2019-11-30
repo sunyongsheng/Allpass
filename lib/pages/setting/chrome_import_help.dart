06344399b2359332378c969de1e83926c4c0cd51
Aengus Sun <sunyongsheng6511@gmail.com>
2019-11-30 16:40:03 +0800

    增加从Chrome导入密码的帮助页

---
+++ lib/pages/setting/chrome_import_help.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+/// 显示如何从Chrome中导入密码
+class ChromeImportHelpPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+}
