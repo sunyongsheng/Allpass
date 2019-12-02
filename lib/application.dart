db5a15e85294095f8b70011f4046ebdbf98c846b
Aengus Sun <sunyongsheng6511@gmail.com>
2019-12-02 20:35:48 +0800

    新增变量SharedPreferences与GetIt，设置两者初始化方法

--- lib/application.dart
+++ lib/application.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
 import 'package:fluro/fluro.dart';
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:allpass/route/navigate_service.dart';
 
 class Application {
   static Router router;
   static GlobalKey<NavigatorState> key = GlobalKey();
-  // static setupLocator(){
-  //   getIt.registerSingleton(NavigateService());
-  // }
+  static SharedPreferences sp;
+  static GetIt getIt = GetIt.asNewInstance();
+
+  static initSp() async {
+    sp = await SharedPreferences.getInstance();
+  }
+
+  static setupLocator() {
+    getIt.registerSingleton(NavigateService());
+  }
 }
