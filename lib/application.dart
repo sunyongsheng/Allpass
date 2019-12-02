36163850553eb8647a6a096a56e283fb74bceabd
Aengus Sun <sunyongsheng6511@gmail.com>
2019-12-02 20:21:46 +0800

    新增应用类，存储一些全局变量

---
+++ lib/application.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:fluro/fluro.dart';
+
+class Application {
+  static Router router;
+  static GlobalKey<NavigatorState> key = GlobalKey();
+  // static setupLocator(){
+  //   getIt.registerSingleton(NavigateService());
+  // }
+}
