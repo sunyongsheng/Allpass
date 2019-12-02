04908190bb9f7b7e95b6fb8d4a5f3069b591dd5d
Aengus Sun <sunyongsheng6511@gmail.com>
2019-12-02 22:20:39 +0800

    采用shared_preferences作为持久化工具

--- lib/params/params.dart
+++ lib/params/params.dart
@@ -1,6 +1,5 @@
-import 'dart:io';
-
-import 'package:path_provider/path_provider.dart';
+import 'package:allpass/application.dart';
+import 'package:allpass/utils/string_process.dart';
 
 /// 存储系统参数
 class Params {
@@ -41,47 +40,62 @@ class Params {
     folderList.clear();
     labelList.clear();
 
-    Directory appDocDir = await getApplicationDocumentsDirectory();
-    appPath = appDocDir.path;
+    // Directory appDocDir = await getApplicationDocumentsDirectory();
+    // appPath = appDocDir.path;
+//
+    // final folderFile = File("$appPath/folder.appt"); // all_pass_plain_text
+    // if (!folderFile.existsSync()) {
+    //   folderFile.createSync();
+    // }
+    // String folder = folderFile.readAsStringSync();
+    // for (String f in folder.split(",")) {
+    //   if (f != "" && f != '~' && f != ',' && f != " ") folderList.add(f);
+    // }
+//
+    // final labelFile = File("$appPath/label.appt");
+    // if (!labelFile.existsSync()) {
+    //   labelFile.createSync();
+    // }
+    // String label = labelFile.readAsStringSync();
+    // for (String l in label.split(",")) {
+    //   if (l != "" && l != '~' && l != ',' && l != " ") labelList.add(l);
+    // }
 
-    final folderFile = File("$appPath/folder.appt"); // all_pass_plain_text
-    if (!folderFile.existsSync()) {
-      folderFile.createSync();
+    // 采用SharedPreferences初始化数据
+    if (Application.sp.containsKey("folder")) {
+      String folder = Application.sp.getString("folder");
+      folderList = str2List(folder);
     }
-    String folder = folderFile.readAsStringSync();
-    for (String f in folder.split(",")) {
-      if (f != "" && f != '~' && f != ',' && f != " ") folderList.add(f);
-    }
-
-    final labelFile = File("$appPath/label.appt");
-    if (!labelFile.existsSync()) {
-      labelFile.createSync();
-    }
-    String label = labelFile.readAsStringSync();
-    for (String l in label.split(",")) {
-      if (l != "" && l != '~' && l != ',' && l != " ") labelList.add(l);
+    if (Application.sp.containsKey("label")) {
+      String label = Application.sp.getString("label");
+      labelList = str2List(label);
     }
   }
 
   /// 标签参数持久化
   static labelParamsPersistence() {
-    final labelFile = File("$appPath/label.appt");
-    String label = "";
-    for (var s in labelList) {
-      label += s;
-      if (s != labelList.last) label += ",";
-    }
-    labelFile.writeAsStringSync(label, flush: true);
+    // 采用SharedPreferences持久化数据
+    Application.sp.setString("label", list2Str(labelList));
+
+    // final labelFile = File("$appPath/label.appt");
+    // String label = "";
+    // for (var s in labelList) {
+    //   label += s;
+    //   if (s != labelList.last) label += ",";
+    // }
+    // labelFile.writeAsStringSync(label, flush: true);
   }
 
   /// 文件夹参数持久化
   static folderParamsPersistence() {
-    final folderFile = File("$appPath/folder.appt");
-    String folder = "";
-    for (var s in folderList) {
-      folder += s;
-      if (s != folderList.last) folder += ",";
-    }
-    folderFile.writeAsStringSync(folder, flush: true);
+    // 采用SharedPreferences持久化数据
+    Application.sp.setString("folder", list2Str(folderList));
+    // final folderFile = File("$appPath/folder.appt");
+    // String folder = "";
+    // for (var s in folderList) {
+    //   folder += s;
+    //   if (s != folderList.last) folder += ",";
+    // }
+    // folderFile.writeAsStringSync(folder, flush: true);
   }
 }
