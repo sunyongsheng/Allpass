29b8e83019e2c5f3b427271799f96453d8ff667e
Aengus Sun <sunyongsheng6511@gmail.com>
2019-12-04 21:23:33 +0800

    修改 指纹登录时的提示信息为中文

--- lib/services/local_authentication_service.dart
+++ lib/services/local_authentication_service.dart
@@ -1,5 +1,7 @@
 import 'dart:io' show Platform;
 import 'package:flutter/services.dart';
+
+import 'package:local_auth/auth_strings.dart';
 import 'package:local_auth/local_auth.dart';
 
 import 'package:allpass/params/params.dart';
@@ -8,6 +10,21 @@ class LocalAuthenticationService {
   final _auth = LocalAuthentication();
 
   bool isAuthenticated = false;
+
+  final androidString = const AndroidAuthMessages(
+    cancelButton: "取消",
+    goToSettingsButton: "设置",
+    goToSettingsDescription: "请设置你的指纹",
+    fingerprintRequiredTitle: "请验证指纹",
+    fingerprintNotRecognized: "指纹识别失败，请重新验证"
+  );
+
+  final iosString = const IOSAuthMessages(
+      cancelButton: "取消",
+      goToSettingsButton: "设置",
+      goToSettingsDescription: "请设置你的指纹",
+      lockOut: "指纹识别失败，请重新验证"
+  );
 
   Future<bool> authenticate() async {
     if (Params.enabledBiometrics) {
@@ -26,6 +43,8 @@ class LocalAuthenticationService {
           localizedReason: '授权以访问账号',
           useErrorDialogs: true,
           stickyAuth: true,
+          androidAuthStrings: androidString,
+          iOSAuthStrings: iosString
         );
         return isAuthenticated;
       } on PlatformException catch (e) {
