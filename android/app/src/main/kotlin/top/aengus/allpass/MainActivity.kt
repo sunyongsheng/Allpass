717f194ae877cf08b4a5d46c5d4f971a10b0e53f
Aengus Sun <sunyongsheng6511@gmail.com>
2019-12-04 13:27:14 +0800

    适配local_auth

--- android/app/src/main/kotlin/top/aengus/allpass/MainActivity.kt
+++ android/app/src/main/kotlin/top/aengus/allpass/MainActivity.kt
@@ -2,10 +2,10 @@ package top.aengus.allpass
 
 import android.os.Bundle
 
-import io.flutter.app.FlutterActivity
+import io.flutter.app.FlutterFragmentActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
