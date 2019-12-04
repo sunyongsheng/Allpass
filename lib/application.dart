dde405e1bac4b34cb80d5260ad5cc9ec1db1cb2e
Aengus Sun <sunyongsheng6511@gmail.com>
2019-12-04 13:12:14 +0800

    注册LocalAuthenticationService单例

--- lib/application.dart
+++ lib/application.dart
@@ -5,6 +5,7 @@ import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:allpass/services/navigate_service.dart';
+import 'package:allpass/services/local_authentication_service.dart';
 
 class Application {
   static Router router;
@@ -18,6 +19,6 @@ class Application {
 
   static setupLocator() {
     getIt.registerSingleton(NavigateService());
-
+    getIt.registerSingleton(LocalAuthenticationService());
   }
 }
