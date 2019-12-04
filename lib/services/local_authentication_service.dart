e47837e1805e7e4dc876c2df846993ecc502a171
Aengus Sun <sunyongsheng6511@gmail.com>
2019-12-04 13:10:36 +0800

    新增 LocalAuthenticationService类，管理生物识别服务

---
+++ lib/services/local_authentication_service.dart
@@ -0,0 +1,37 @@
+import 'dart:io' show Platform;
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+
+import 'package:allpass/params/params.dart';
+
+class LocalAuthenticationService {
+  final _auth = LocalAuthentication();
+
+  bool isAuthenticated = false;
+
+  Future<bool> authenticate() async {
+    if (Params.enabledBiometrics) {
+      try {
+        List<BiometricType> availableBiometrics =
+        await _auth.getAvailableBiometrics();
+
+        if (Platform.isIOS) {
+          if (availableBiometrics.contains(BiometricType.face)) {
+            // Face ID.
+          } else if (availableBiometrics.contains(BiometricType.fingerprint)) {
+            // Touch ID.
+          }
+        }
+        isAuthenticated = await _auth.authenticateWithBiometrics(
+          localizedReason: 'authenticate to access',
+          useErrorDialogs: true,
+          stickyAuth: true,
+        );
+        return isAuthenticated;
+      } on PlatformException catch (e) {
+        print(e);
+      }
+    }
+    return false;
+  }
+}
