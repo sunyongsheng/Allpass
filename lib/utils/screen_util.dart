411a23534890bced5c2aaaf1dba2aad364b89256
Aengus Sun <sunyongsheng6511@gmail.com>
2019-12-10 18:27:46 +0800

    新增AllpassScreenUtil类，用于设置与屏幕尺寸相关的参数

---
+++ lib/utils/screen_util.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AllpassScreenUtil {
+
+  static ScreenUtil _instance = ScreenUtil.getInstance();
+
+  static num setWidth(num width) {
+    return _instance.setWidth(width);
+  }
+
+  static num setHeight(num height) {
+    return _instance.setHeight(height);
+  }
+}
