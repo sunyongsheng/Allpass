cae627d7ddda6b23f39b92ca1b2a0a61fdf84fd6
Aengus Sun <sunyongsheng6511@gmail.com>
2019-12-10 17:14:02 +0800

    提取登录与注册函数，对主账号密码进行加密存储

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -8,7 +8,7 @@ import 'package:allpass/dao/card_dao.dart';
 import 'package:allpass/dao/password_dao.dart';
 import 'package:allpass/utils/allpass_ui.dart';
 import 'package:allpass/utils/navigation_util.dart';
-
+import 'package:allpass/utils/encrypt_helper.dart';
 
 /// 登陆页面
 class LoginPage extends StatefulWidget {
@@ -88,30 +88,7 @@ class _LoginPage extends State<LoginPage> {
               padding: EdgeInsets.only(left: 10, right: 10, top: 8, bottom: 8),
               child: Text("登录", style: TextStyle(color: Colors.white, fontSize: 16)),
               color: AllpassColorUI.mainColor,
-              onPressed: () async {
-                if (inputErrorTimes >= 5) {
-                  PasswordDao().deleteContent();
-                  CardDao().deleteContent();
-                  Params.paramsClear();
-                  await Application.sp.clear();
-                  Fluttertoast.showToast(msg: "连续错误超过五次！已清除所有数据，请重新注册");
-                } else {
-                  if (Params.username != "" && Params.password != "") {
-                    if (Params.username == _usernameController.text
-                        && Params.password == _passwordController.text) {
-                      NavigationUtil.goHomePage(context);
-                      Fluttertoast.showToast(msg: "登录成功");
-                    } else if (_usernameController.text == "" || _passwordController.text == "") {
-                      Fluttertoast.showToast(msg: "请输入用户名或密码");
-                    } else {
-                      inputErrorTimes++;
-                      Fluttertoast.showToast(msg: "用户名或密码错误，已错误$inputErrorTimes次，连续超过五次将删除所有数据！");
-                    }
-                  } else {
-                    Fluttertoast.showToast(msg: "当前不存在用户，请先注册！");
-                  }
-                }
-              },
+              onPressed: () => login(),
             ),
             Padding(
               padding: EdgeInsets.only(top: 80, bottom: 80),
@@ -121,25 +98,7 @@ class _LoginPage extends State<LoginPage> {
               children: <Widget>[
                 FlatButton(
                   child: Text("注册"),
-                  onPressed: () {
-                    // 判断是否已有账号存在
-                    if (Application.sp.getString("username") == null) {
-                      // 判断用户名和密码长度
-                      if (_usernameController.text.length >= 6 && _passwordController.text.length >= 6) {
-                        Application.sp.setString("username", _usernameController.text);
-                        Application.sp.setString("password", _passwordController.text);
-                        Params.username = _usernameController.text;
-                        Params.password = _passwordController.text;
-                        Params.enabledBiometrics = false;
-                        inputErrorTimes = 0;
-                        Fluttertoast.showToast(msg: "注册成功");
-                      } else {
-                        Fluttertoast.showToast(msg: "用户名或密码长度必须大于等于6！");
-                      }
-                    } else {
-                      Fluttertoast.showToast(msg: "已有账号注册过，只允许单账号");
-                    }
-                  },
+                  onPressed: () => register(),
                 ),
                 Text("|"),
                 FlatButton(
@@ -154,6 +113,52 @@ class _LoginPage extends State<LoginPage> {
         ),
       ),
     );
+  }
+
+  void register() async {
+    // 判断是否已有账号存在
+    if (Application.sp.getString("username") == null) {
+      // 判断用户名和密码长度
+      if (_usernameController.text.length >= 6 && _passwordController.text.length >= 6) {
+        String _password = await EncryptHelper.encrypt(_passwordController.text);
+        Application.sp.setString("username", _usernameController.text);
+        Application.sp.setString("password", _password);
+        Params.username = _usernameController.text;
+        Params.password = _password;
+        Params.enabledBiometrics = false;
+        inputErrorTimes = 0;
+        Fluttertoast.showToast(msg: "注册成功");
+      } else {
+        Fluttertoast.showToast(msg: "用户名或密码长度必须大于等于6！");
+      }
+    } else {
+      Fluttertoast.showToast(msg: "已有账号注册过，只允许单账号");
+    }
+  }
+
+  login() async {
+    if (inputErrorTimes >= 5) {
+      PasswordDao().deleteContent();
+      CardDao().deleteContent();
+      Params.paramsClear();
+      await Application.sp.clear();
+      Fluttertoast.showToast(msg: "连续错误超过五次！已清除所有数据，请重新注册");
+    } else {
+      if (Params.username != "" && Params.password != "") {
+        if (Params.username == _usernameController.text
+            && Params.password == await EncryptHelper.encrypt(_passwordController.text)) {
+          NavigationUtil.goHomePage(context);
+          Fluttertoast.showToast(msg: "登录成功");
+        } else if (_usernameController.text == "" || _passwordController.text == "") {
+          Fluttertoast.showToast(msg: "请输入用户名或密码");
+        } else {
+          inputErrorTimes++;
+          Fluttertoast.showToast(msg: "用户名或密码错误，已错误$inputErrorTimes次，连续超过五次将删除所有数据！");
+        }
+      } else {
+        Fluttertoast.showToast(msg: "当前不存在用户，请先注册！");
+      }
+    }
   }
 
 }
