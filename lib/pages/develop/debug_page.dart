0f4a144b597e3c14351cf5287f98969b43686c08
Aengus Sun <sunyongsheng6511@gmail.com>
2019-12-11 13:34:45 +0800

    新增 调试页面

---
+++ lib/pages/develop/debug_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'package:allpass/dao/password_dao.dart';
+import 'package:allpass/dao/card_dao.dart';
+
+/// 调试页
+class DebugPage extends StatefulWidget {
+  @override
+  _DebugPage createState() {
+    return _DebugPage();
+  }
+
+}
+
+class _DebugPage extends State<DebugPage> {
+
+  final PasswordDao _passwordDao = PasswordDao();
+  final CardDao _cardDao = CardDao();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DEBUG MODE"),
+        automaticallyImplyLeading: false,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: FlatButton(
+                child: Text("删除所有密码记录"),
+                onPressed: () async {
+                  await _passwordDao.deleteContent();
+                  Fluttertoast.showToast(msg: "已删除所有密码");
+                },
+              ),
+            ),
+            ListTile(
+              title: FlatButton(
+                child: Text("删除所有卡片记录"),
+                onPressed: () async {
+                  await _cardDao.deleteContent();
+                  Fluttertoast.showToast(msg: "已删除所有卡片");
+                },
+              ),
+            ),
+            ListTile(
+              title: FlatButton(
+                child: Text("删除密码数据库"),
+                onPressed: () async {
+                  await _passwordDao.deleteTable();
+                  Fluttertoast.showToast(msg: "已删除密码数据库");
+                },
+              ),
+            ),
+            ListTile(
+              title: FlatButton(
+                child: Text("删除卡片数据库"),
+                onPressed: () async {
+                  await _cardDao.deleteTable();
+                  Fluttertoast.showToast(msg: "已删除卡片数据库");
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
