1549d39f58675ed04c96783d917858c6892cd406
Aengus Sun <sunyongsheng6511@gmail.com>
2019-12-28 14:13:04 +0800

    新增类用于存储app中所有的password数据与card数据

---
+++ lib/provider/card_list.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import 'package:allpass/dao/card_dao.dart';
+import 'package:allpass/model/card_bean.dart';
+
+/// 保存程序中所有的Card
+class CardList with ChangeNotifier {
+  List<CardBean> _cardList = [];
+
+  CardDao _dao = CardDao();
+
+  List<CardBean> get cardList =>_cardList;
+
+  CardList() {
+    init();
+  }
+
+  init() async {
+    _cardList = await _dao.getAllCardBeanList();
+  }
+
+  insertCard(CardBean bean) async {
+    _cardList.add(bean);
+    await _dao.insert(bean);
+    notifyListeners();
+  }
+
+  deleteCard(CardBean bean) async {
+    _cardList.remove(bean);
+    await _dao.deleteCardBeanById(bean.uniqueKey);
+    notifyListeners();
+  }
+
+  updateCard(CardBean bean) async {
+    int index = -1;
+    for (int i = 0; i < _cardList.length; i++) {
+      if (_cardList[i].uniqueKey == bean.uniqueKey) {
+        index = i;
+        break;
+      }
+    }
+    _cardList[index] = bean;
+    await _dao.updatePasswordBean(bean);
+    notifyListeners();
+  }
+
+  clear() async {
+    _cardList?.clear();
+    await _dao.deleteContent();
+    notifyListeners();
+  }
+}
---
+++ lib/provider/password_list.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'package:allpass/dao/password_dao.dart';
+import 'package:allpass/model/password_bean.dart';
+
+/// 保存程序中的所有的Password
+class PasswordList with ChangeNotifier {
+  List<PasswordBean> _passwordList = [];
+  PasswordDao _dao = PasswordDao();
+
+  List<PasswordBean> get passwordList => _passwordList;
+
+  PasswordList() {
+    init();
+  }
+
+  init() async {
+    _passwordList = await _dao.getAllPasswordBeanList();
+  }
+
+  insertPassword(PasswordBean bean) async {
+    _passwordList?.add(bean);
+    await _dao.insert(bean);
+    notifyListeners();
+  }
+
+  deletePassword(PasswordBean bean) async {
+    _passwordList?.remove(bean);
+    await _dao.deletePasswordBeanById(bean.uniqueKey);
+    notifyListeners();
+  }
+
+  updatePassword(PasswordBean bean) async {
+    int index = -1;
+    for (int i = 0; i < _passwordList.length; i++) {
+      if (_passwordList[i].uniqueKey == bean.uniqueKey) {
+        index = i;
+        break;
+      }
+    }
+    _passwordList[index] = bean;
+    await _dao.updatePasswordBean(bean);
+    notifyListeners();
+  }
+
+  clear() async {
+    _passwordList?.clear();
+    await _dao.deleteContent();
+    notifyListeners();
+  }
+}
