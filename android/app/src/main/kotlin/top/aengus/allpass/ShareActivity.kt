5f91256c17bb02a6db2efeb802be5ccb325a0c70
Aengus Sun <sunyongsheng6511@gmail.com>
2019-12-29 21:48:29 +0800

    接收分享文件（未完成）

--- android/app/src/main/kotlin/top/aengus/allpass/ShareActivity.kt
+++ android/app/src/main/kotlin/top/aengus/allpass/ShareActivity.kt
@@ -1,5 +1,28 @@
 package top.aengus.allpass
 
-class ShareActivity {
+import android.os.Bundle
+import android.content.Intent
 
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class ShareActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent: Intent = intent
+        val action: String? = intent.action
+        val type: String? = intent.type
+        if (Intent.ACTION_SEND == (type)) {
+            if ("text/plain" == type) {
+                dealTextMessage(intent)
+            }
+        }
+    }
+
+    private fun dealTextMessage(intent: Intent) {
+        val share: String? = intent.getStringExtra(Intent.EXTRA_TEXT)
+        val title: String? = intent.getStringExtra(Intent.EXTRA_TITLE)
+        print(share)
+        print(title)
+    }
 }
