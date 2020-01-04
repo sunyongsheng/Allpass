ecdb87d95e51b5141adcc1814fc4d743bb5e4d75
Aengus Sun <sunyongsheng6511@gmail.com>
2020-01-04 12:49:57 +0800

    feat(lib/widgets): 新增确认框组件
    通过在构造参数中指定信息，返回圆角确认对话框，并pop时传递bool值以返回点击按钮结果

---
+++ lib/widgets/confirm_dialog.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ConfirmDialog extends StatelessWidget {
+  final String message;
+  ConfirmDialog(this.message);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius:
+        BorderRadius.all(Radius.circular(15),),
+      ),
+      title: Text("确认删除"),
+      content: Text(message),
+      actions: <Widget>[
+        FlatButton(
+          child: Text("确认"),
+          onPressed: () async {
+            Navigator.pop<bool>(context, true);
+          },
+        ),
+        FlatButton(
+          child: Text("取消"),
+          onPressed: () {
+            Navigator.pop<bool>(context, false);
+          },
+        )
+      ],
+    );
+  }
+
+
+}
