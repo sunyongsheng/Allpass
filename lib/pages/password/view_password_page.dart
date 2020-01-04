e28326c7063ff23071f0ce942fb434c3d0f070ac
Aengus Sun <sunyongsheng6511@gmail.com>
2020-01-04 11:21:07 +0800

    修改查看密码页面样式（未完成）

--- lib/pages/password/view_password_page.dart
+++ lib/pages/password/view_password_page.dart
@@ -1,9 +1,16 @@
-import 'package:allpass/params/params.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'package:allpass/params/params.dart';
 import 'package:allpass/model/password_bean.dart';
 import 'package:allpass/utils/allpass_ui.dart';
+import 'package:allpass/utils/encrypt_util.dart';
+import 'package:allpass/utils/screen_util.dart';
+import 'package:allpass/pages/password/view_and_edit_password_page.dart';
 
 /// 查看密码页
 class ViewPasswordPage extends StatefulWidget {
@@ -15,16 +22,15 @@ class ViewPasswordPage extends StatefulWidget {
   State<StatefulWidget> createState() {
     return _ViewPasswordPage(oldData);
   }
-
 }
 
 class _ViewPasswordPage extends State<ViewPasswordPage> {
-
   PasswordBean _oldData;
   PasswordBean _tempData;
 
   bool _passwordVisible = false;
 
+  String _password = "";
 
   _ViewPasswordPage(PasswordBean data) {
     this._oldData = data;
@@ -51,90 +57,263 @@ class _ViewPasswordPage extends State<ViewPasswordPage> {
     }
   }
 
+  Future<Null> _decryptPassword() async {
+    _password =  await EncryptUtil.decrypt(_tempData.password);
+  }
+
+  @override
+  void initState() {
+    _decryptPassword();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("查看密码", style: AllpassTextUI.mainTitleStyle,),
-        centerTitle: true,
+        appBar: AppBar(
+          title: Text(
+            "查看密码",
+            style: AllpassTextUI.titleBarStyle,
+          ),
+          centerTitle: true,
+          backgroundColor: AllpassColorUI.mainBackgroundColor,
+          iconTheme: IconThemeData(color: Colors.black),
+          elevation: 0,
+          brightness: Brightness.light,
+        ),
         backgroundColor: AllpassColorUI.mainBackgroundColor,
-        elevation: 0,
-        brightness: Brightness.light,
-      ),
-      backgroundColor: AllpassColorUI.mainBackgroundColor,
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 150,
-                width: 330,
-                margin: EdgeInsets.only(bottom: 20, top: 10),
-                child: Card(
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Card(
+                  margin: EdgeInsets.only(
+                    top: AllpassScreenUtil.setHeight(50),
+                      left: AllpassScreenUtil.setWidth(80),
+                      right: AllpassScreenUtil.setWidth(80),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0))),
                   color: AllpassColorUI.mainBackgroundColor,
                   elevation: 8,
-                  child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.all(20),
-                          child: CircleAvatar(
-                            radius: 30,
-                            backgroundColor: getRandomColor(_tempData.uniqueKey),
-                            child: Text(_tempData.name.substring(0, 1), style: TextStyle(fontSize: 40, color: Colors.white),),
-                          ),
-                        ),
-                        Column(
+                  child: SizedBox(
+                      width: ScreenUtil.screenWidth * 0.8,
+                      child: Center(
+                        child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
                             Padding(
-                              padding: EdgeInsets.only(bottom: 0),
-                              child: Text(_tempData.name, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                              padding: EdgeInsets.all(20),
+                              child: CircleAvatar(
+                                radius: 30,
+                                backgroundColor:
+                                    getRandomColor(_tempData.uniqueKey),
+                                child: Text(
+                                  _tempData.name.substring(0, 1),
+                                  style: TextStyle(
+                                      fontSize: 30, color: Colors.white),
+                                ),
+                              ),
                             ),
-                            Wrap(
-                                crossAxisAlignment: WrapCrossAlignment.start,
-                                spacing: 4.0,
-                                runSpacing: 5.0,
-                                children: _getTag()),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.only(left: 5),
+                                  child: Text(
+                                    _tempData.name,
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Container(
+                                    margin: EdgeInsets.only(
+                                        left: 5, right: 5, top: 3, bottom: 3),
+                                    child: Row(
+                                      children: <Widget>[
+                                        Icon(Icons.folder_open),
+                                        Container(
+                                          margin: EdgeInsets.only(left: 5),
+                                          color: Colors.grey[250],
+                                          child: Text(_tempData.folder),
+                                        ),
+                                      ],
+                                    )),
+                              ],
+                            )
                           ],
-                        )
-                      ],
-                    ),
-                  )
+                        ),
+                      )),
                 ),
-              ),
-              Container(
-                height: 400,
-                width: 330,
-                margin: EdgeInsets.only(bottom: 0, top: 10),
-                child: Card(
-                  color: AllpassColorUI.mainBackgroundColor,
-                  elevation: 8,
-                  child: Column(
-                    children: <Widget>[
-                      Text(_tempData.name, style: AllpassTextUI.mainTitleStyle,),
-                    ],
-                  ),
+                Padding(
+                  padding: AllpassEdgeInsets.smallTBPadding,
                 ),
-              ),
-              FloatingActionButton(
-                onPressed: () {},
-                child: Icon(Icons.edit),
-              ),
-            ],
+                Card(
+                    margin: EdgeInsets.only(
+                        left: AllpassScreenUtil.setWidth(80),
+                        right: AllpassScreenUtil.setWidth(80)),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8.0))),
+                    color: AllpassColorUI.mainBackgroundColor,
+                    elevation: 8,
+                    child: SizedBox(
+                      width: ScreenUtil.screenWidth * 0.8,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(padding: AllpassEdgeInsets.smallTBPadding,),
+                          Container(
+                            margin: EdgeInsets.only(
+                              left: AllpassScreenUtil.setWidth(100),
+                              right: AllpassScreenUtil.setWidth(100),
+                              bottom: AllpassScreenUtil.setHeight(10)
+                            ),
+                            child: Text("用户名", style: AllpassTextUI.firstTitleStyleBlue,),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(
+                                left: AllpassScreenUtil.setWidth(100),
+                                right: AllpassScreenUtil.setWidth(100),
+                                bottom: AllpassScreenUtil.setHeight(50)
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Expanded(
+                                  child: Text(_tempData.username,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: AllpassTextUI.firstTitleStyleBlack,
+                                  ),
+                                ),
+                                Padding(padding: AllpassEdgeInsets.smallLPadding,),
+                                InkWell(
+                                  child: Text("复制", style: AllpassTextUI.secondTitleStyleBlue,),
+                                  onTap: () {
+                                    Clipboard.setData(ClipboardData(text: _tempData.username));
+                                    Fluttertoast.showToast(msg: "已复制用户名");
+                                  },
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(
+                              left: AllpassScreenUtil.setWidth(100),
+                              right: AllpassScreenUtil.setWidth(100),
+                              bottom: AllpassScreenUtil.setHeight(10)
+                            ),
+                            child: Text("密码", style: AllpassTextUI.firstTitleStyleBlue,),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(
+                                left: AllpassScreenUtil.setWidth(100),
+                                right: AllpassScreenUtil.setWidth(100),
+                                bottom: AllpassScreenUtil.setHeight(30)
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Expanded(
+                                  child: Text(_passwordVisible?_password:"*"*_password.length,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: AllpassTextUI.firstTitleStyleBlack,
+                                  ),
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    InkWell(
+                                      child: _passwordVisible
+                                          ?Icon(Icons.visibility)
+                                          :Icon(Icons.visibility_off),
+                                      onTap: () {
+                                        setState(() {
+                                          _passwordVisible = !_passwordVisible;
+                                        });
+                                      },
+                                    ),
+                                    Padding(padding: AllpassEdgeInsets.smallLPadding,),
+                                    InkWell(
+                                      onTap: () {
+                                        Clipboard.setData(ClipboardData(text: _password));
+                                        Fluttertoast.showToast(msg: "已复制密码");
+                                      },
+                                      child: Text("复制", style: AllpassTextUI.secondTitleStyleBlue,),)
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(
+                              left: AllpassScreenUtil.setWidth(100),
+                              right: AllpassScreenUtil.setWidth(100),
+                              bottom: AllpassScreenUtil.setHeight(10)
+                            ),
+                            child: Text("链接", style: AllpassTextUI.firstTitleStyleBlue,),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(
+                                left: AllpassScreenUtil.setWidth(100),
+                                right: AllpassScreenUtil.setWidth(100),
+                                bottom: AllpassScreenUtil.setHeight(50)
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Expanded(
+                                  child: InkWell(
+                                    onTap: () async {
+                                      if (_tempData.url.startsWith("https")
+                                          || _tempData.url.startsWith("http"))
+                                        await launch(_tempData.url);
+                                    },
+                                    child: Text(_tempData.url,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: AllpassTextUI.firstTitleStyleBlack,
+                                    ),
+                                  )
+                                ),
+                                Padding(padding: AllpassEdgeInsets.smallLPadding,),
+                                InkWell(
+                                  onTap: () {
+                                    Clipboard.setData(ClipboardData(text: _tempData.url));
+                                    Fluttertoast.showToast(msg: "已复制链接");
+                                  },
+                                  child: Text("复制", style: AllpassTextUI.secondTitleStyleBlue,),)
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+                Padding(
+                  padding: AllpassEdgeInsets.smallTBPadding,
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) => ViewAndEditPasswordPage(_tempData, "编辑密码", false)
+                    ));
+                  },
+                  child: Icon(Icons.edit),
+                ),
+              ],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 
   List<Widget> _getTag() {
     List<Widget> labelChoices = List();
     _tempData.label.forEach((item) {
       labelChoices.add(ChoiceChip(
-        label: Text(item, style: TextStyle(fontSize: 10),),
+        label: Text(
+          item,
+          style: TextStyle(fontSize: 10),
+        ),
         labelStyle: AllpassTextUI.secondTitleStyleBlack,
         selected: _tempData.label.contains(item),
         onSelected: (selected) {},
