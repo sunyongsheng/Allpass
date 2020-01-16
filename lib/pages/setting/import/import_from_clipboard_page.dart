e205efaaabf10791ee920d163a1d21782cee9ec1
Aengus Sun <sunyongsheng6511@gmail.com>
2020-01-16 18:22:44 +0800

    feat(lib/pages/setting/import): 增加帮助按钮

--- lib/pages/setting/import/import_from_clipboard_page.dart
+++ lib/pages/setting/import/import_from_clipboard_page.dart
@@ -34,6 +34,34 @@ class _ImportFromClipboard extends State<ImportFromClipboard> {
           backgroundColor: AllpassColorUI.mainBackgroundColor,
           iconTheme: IconThemeData(color: Colors.black),
           elevation: 0,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.help_outline, color: Colors.black,),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text("帮助"),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                    ),
+                    content: SingleChildScrollView(
+                      child: ListBody(
+                        children: <Widget>[
+                          Text("此功能帮助您轻松地从之前在记事本中保存的密码导入到Allpass中；\n"),
+                          Text("名称是密码的助记符，您可以随便请一个名称来让您知道此条记录是什么内容；\n"),
+                          Text("用户名是登录使用的账号名，有可能是手机、邮箱或者其他您设置的账号；\n"),
+                          Text("网站地址可以帮助Allpass在正确的网站填充您的密码，大多数情况下是网站登录页的URL地址；\n"),
+                          Text("两个字段之间请以“空格”作为分隔符，这样Allpass才能正确分辨哪个是用户名，哪个是密码；\n"),
+                          Text("如果选择了最后一个导入格式，请在第一行输入统一的用户名；如果有多个用户名，可以分为几次导入；")
+                        ],
+                      )
+                    ),
+                  )
+                );
+              },
+            ),
+          ],
         ),
         backgroundColor: AllpassColorUI.mainBackgroundColor,
         body: SingleChildScrollView(
