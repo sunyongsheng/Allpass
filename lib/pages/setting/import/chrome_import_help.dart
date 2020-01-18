d96744888855cd32e02135fe9919fe41bd09b548
Aengus Sun <sunyongsheng6511@gmail.com>
2020-01-18 21:51:03 +0800

    feat(lib/pages/setting/import): 完成从Chrome中导入帮助页

--- lib/pages/setting/import/chrome_import_help.dart
+++ lib/pages/setting/import/chrome_import_help.dart
@@ -1,14 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:allpass/utils/allpass_ui.dart';
 
 /// 显示如何从Chrome中导入密码
 class ChromeImportHelpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text("对不起，这个功能不会做"),
+        appBar: AppBar(
+          title: Text(
+            "从Chrome中导入",
+            style: AllpassTextUI.firstTitleStyleBlack,
+          ),
+          backgroundColor: Colors.white,
+          iconTheme: IconThemeData(color: Colors.black),
+          elevation: 0,
+          brightness: Brightness.light,
         ),
-      ));
+      backgroundColor: AllpassColorUI.mainBackgroundColor,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: AllpassEdgeInsets.listInset,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("1. 打开手机Chrome，点击“菜单”", style: AllpassTextUI.titleBarStyle,),
+              Image.asset("assets/img/import_step1.jpg", width: ScreenUtil.screenWidth*0.5,),
+              Padding(padding: AllpassEdgeInsets.smallTBPadding,),
+              Divider(thickness: 2,),
+              Text("2. 点击“设置”", style: AllpassTextUI.titleBarStyle,),
+              Image.asset("assets/img/import_step2.jpg"),
+              Padding(padding: AllpassEdgeInsets.smallTBPadding,),
+              Divider(thickness: 2,),
+              Text("3. 点击“密码”", style: AllpassTextUI.titleBarStyle,),
+              Image.asset("assets/img/import_step3.jpg"),
+              Padding(padding: AllpassEdgeInsets.smallTBPadding,),
+              Divider(thickness: 2,),
+              Text("4. 点击右上角，选择”导出密码“", style: AllpassTextUI.titleBarStyle,),
+              Image.asset("assets/img/import_step4.jpg"),
+              Padding(padding: AllpassEdgeInsets.smallTBPadding,),
+              Divider(thickness: 2,),
+              Text("5. 选择”导入到Allpass中“，稍作等待，即可完成", style: AllpassTextUI.titleBarStyle,),
+              Image.asset("assets/img/import_step5.png")
+            ],
+          ),
+        ),
+      )
+    );
   }
 }
