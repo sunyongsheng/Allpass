3e2ec1fc5c7f124a0f5dc4d9d8f3b1525cd4d5ae
Aengus Sun <sunyongsheng6511@gmail.com>
2020-01-25 21:50:59 +0800

    feat(lib/widgets): 新增 输入主密码对话框
    返回bool代表输入密码是否正确

---
+++ lib/widgets/input_main_password_dialog.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'package:allpass/params/params.dart';
+import 'package:allpass/utils/allpass_ui.dart';
+import 'package:allpass/utils/encrypt_util.dart';
+
+class InputMainPasswordDialog extends StatelessWidget {
+
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(15),
+        ),
+      ),
+      title: Text("请输入主密码"),
+      content: TextField(
+        controller: _passwordController,
+        obscureText: true,
+        autofocus: true,
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text("确认", style: AllpassTextUI.secondTitleStyleBlue,),
+          onPressed: () async {
+            if (await EncryptUtil.encrypt(_passwordController.text) == Params.password) {
+              _passwordController.clear();
+              Navigator.pop<bool>(context, true);
+            } else {
+              Fluttertoast.showToast(msg: "密码错误");
+              _passwordController.clear();
+              Navigator.pop<bool>(context, false);
+            }
+          },
+        ),
+        FlatButton(
+          child: Text("取消", style: AllpassTextUI.secondTitleStyleBlue,),
+          onPressed: () => Navigator.pop<bool>(context, false),
+        )
+      ],
+    );
+  }
+
+}
