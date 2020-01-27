a5f67b2a538b758fb323fd8cb000b0b78d94e97a
Aengus Sun <sunyongsheng6511@gmail.com>
2020-01-27 10:44:23 +0800

    refactor(lib): 应用初始化文件夹“学习”改为“教育”

--- lib/application.dart
+++ lib/application.dart
@@ -60,6 +60,6 @@ class Application {
 }
 
 void initApp() {
-  Application.sp?.setString("folder", "默认~娱乐~办公~论坛~学习~社交");
+  Application.sp?.setString("folder", "默认~娱乐~办公~论坛~教育~社交");
   Application.sp?.setBool("FIRST_RUN", false);
 }
