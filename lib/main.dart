5f8463b5617b9367c0f158c6af61345d4d937420
Aengus Sun <sunyongsheng6511@gmail.com>
2020-01-31 20:04:37 +0800

    feat(lib): 添加自定义错误界面

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:allpass/pages/login/auth_login_page.dart';
 import 'package:allpass/params/params.dart';
 import 'package:allpass/application.dart';
 import 'package:allpass/route/routes.dart';
+import 'package:allpass/utils/allpass_ui.dart';
 import 'package:allpass/provider/card_list.dart';
 import 'package:allpass/provider/password_list.dart';
 
@@ -30,6 +31,32 @@ void main() async {
         SystemUiOverlayStyle(statusBarColor: Colors.transparent);
     SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
   }
+
+  // 自定义报错页面
+  ErrorWidget.builder = (FlutterErrorDetails flutterErrorDetails) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          child: Text("App出现错误，快去反馈给作者!"),
+          padding: AllpassEdgeInsets.smallTBPadding,
+        ),
+        Padding(
+          padding: AllpassEdgeInsets.smallTBPadding,
+        ),
+        Padding(
+          child: Text("以下是出错信息，请截图发到邮箱sys6511@126.com"),
+          padding: AllpassEdgeInsets.smallTBPadding,
+        ),
+        Padding(
+          child: Text(flutterErrorDetails.toString()),
+          padding: AllpassEdgeInsets.smallTBPadding,
+        ),
+      ],
+    );
+  };
+
   final PasswordList passwords = PasswordList()..init();
   final CardList cards = CardList()..init();
   runApp(MultiProvider(
