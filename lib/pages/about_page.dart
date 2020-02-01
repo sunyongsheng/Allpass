70ba23055653c98191ccf8bdc8bf0d159ccfa4e3
Aengus Sun <sunyongsheng6511@gmail.com>
2020-02-01 12:16:04 +0800

    feat(lib/pages): 添加 服务条款 按钮

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -8,6 +8,18 @@ import 'package:allpass/utils/screen_util.dart';
 import 'package:allpass/pages/develop/debug_page.dart';
 
 class AboutPage extends StatelessWidget {
+
+  final Text _service = Text(
+    '''
+        1. Allpass（下称“本产品”）是一款开源的私密数据管理工具，采用Apache 2.0协议，所以你可以在满足Apache 2.0协议的基础上对本产品进行再发布。
+        2. 本产品不做任何担保。由于用户行为（Root等）导致用户信息泄露或丢失，本产品免责。
+        3. 任何由于黑客攻击、计算机病毒侵入或发作、因政府管制而造成的暂时性关闭等影响网络正常经营的不可抗力而造成的个人资料泄露、丢失、被盗用或被窜改等，本产品均得免责。
+        4. 使用者因为违反本声明的规定而触犯中华人民共和国法律的，一切后果自己负责，本产品不承担任何责任。
+        5. 开发者不会向任何无关第三方提供、出售、出租、分享或交易您的个人信息。Allpass也不会收集普通用户的信息。
+        ''',
+    style: AllpassTextUI.firstTitleStyleBlack,
+  );
+
   @override
   Widget build(BuildContext context) {
     int pressTimes = 0;
@@ -118,9 +130,64 @@ class AboutPage extends StatelessWidget {
               ],
             ),
           ),
-          Text("Copyright 2020 Aengus Sun. Apache License 2.0", style: TextStyle(color: Colors.grey),)
+          Text("Copyright 2020 Aengus Sun. Apache License 2.0", style: TextStyle(color: Colors.grey),),
+          Padding(
+            padding: EdgeInsets.only(top: AllpassScreenUtil.setHeight(300)),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text("服务条款"),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => DetailsPage(
+                      title: "服务条款",
+                      content: _service,
+                    )
+                  ));
+                },
+              ),
+            ],
+          )
         ],
       )
+    );
+  }
+}
+
+class DetailsPage extends StatelessWidget {
+  final String title;
+  final Widget content;
+
+  const DetailsPage({this.title, this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          title,
+          style: AllpassTextUI.titleBarStyle,
+        ),
+        centerTitle: true,
+        backgroundColor: AllpassColorUI.mainBackgroundColor,
+        iconTheme: IconThemeData(color: Colors.black),
+        elevation: 0,
+        brightness: Brightness.light,
+      ),
+      backgroundColor: AllpassColorUI.mainBackgroundColor,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              child: content,
+              padding: AllpassEdgeInsets.listInset,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
