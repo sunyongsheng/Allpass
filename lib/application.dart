dd08cb1348b80b866eb937895f93a7266423ce62
Aengus Sun <sunyongsheng6511@gmail.com>
2020-02-04 18:59:47 +0800

    更改应用版本

--- lib/application.dart
+++ lib/application.dart
@@ -20,7 +20,7 @@ class Application {
   static GetIt getIt = GetIt.instance;
   static const platform = const MethodChannel("allpass.aengus.top/share");
 
-  static String version = "1.0.0";
+  static String version = "1.0.5";
 
   static initSp() async {
     sp = await SharedPreferences.getInstance();
