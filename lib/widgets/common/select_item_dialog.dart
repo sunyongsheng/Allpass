2254f02bf5e25301d5e2be32333db9a8031ef6ad
Aengus Sun <sunyongsheng6511@gmail.com>
2020-02-07 13:40:47 +0800

    feat(lib/widgets/common): 新增 选择项目对话框
    用于选择某个文件夹，返回此选择文件夹的名称，否则返回null

---
+++ lib/widgets/common/select_item_dialog.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:allpass/params/params.dart';
+import 'package:allpass/utils/allpass_ui.dart';
+
+class SelectItemDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("请选择"),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(AllpassUI.smallBorderRadius)
+      ),
+      content: SingleChildScrollView(
+        child: Column(
+          children: _getList(context)
+        ),
+      )
+    );
+  }
+
+  List<Widget> _getList(BuildContext context) {
+    List<Widget> list = List();
+    for (String f in Params.folderList) {
+      list.add(ListTile(
+        title: Text(f),
+        onTap: () => Navigator.pop<String>(context, f),
+      ));
+    }
+    return list;
+  }
+
+}
