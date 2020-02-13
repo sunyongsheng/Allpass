91bd5fb2c9e5f579adb57aee69eac96927644cfb
Aengus Sun <sunyongsheng6511@gmail.com>
2020-02-13 18:59:14 +0800

    feat(lib/widgets/common): 新增 LabelChip：拥有固定样式的标签

---
+++ lib/widgets/common/label_chip.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:allpass/provider/theme_provider.dart';
+
+class LabelChip extends StatelessWidget {
+  final String text;
+  final bool selected;
+  final void Function(bool) onSelected;
+  const LabelChip({this.text, this.selected, this.onSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChoiceChip(
+      label: Text(text),
+      labelStyle: TextStyle(fontSize: 14, color: Provider.of<ThemeProvider>(context).chipTextColor),
+      selected: selected,
+      onSelected: onSelected,
+      selectedColor: Provider.of<ThemeProvider>(context).currTheme.primaryColor,
+    );
+  }
+
+}
