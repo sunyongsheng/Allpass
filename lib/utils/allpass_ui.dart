f6c7e4a8d5ba3a28f2ca3fc3fb3db8e0b745a1b4
Aengus Sun <sunyongsheng6511@gmail.com>
2020-03-06 17:25:04 +0800

    优化暗黑模式下 下拉框背景颜色

--- lib/utils/allpass_ui.dart
+++ lib/utils/allpass_ui.dart
@@ -244,55 +244,55 @@ class AllpassTheme {
   }
   static ThemeData darkTheme() {
     return ThemeData(
-        primaryColor: Colors.blueAccent,
-        primarySwatch: Colors.blue,
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-            foregroundColor: Colors.white
-        ),
-        appBarTheme: AppBarTheme(
-          brightness: Brightness.dark,
-          iconTheme: IconThemeData(color: Colors.white70),
-          elevation: 0,
-          color: Colors.grey[900],
-        ),
-        iconTheme: IconThemeData(
-            color: Colors.grey[200]
-        ),
-        scaffoldBackgroundColor: Colors.grey[900],
-        cardColor: Colors.black,
-        indicatorColor: Colors.blueAccent,
-        cursorColor: Colors.blueAccent,
-        accentColor: Colors.grey,
-        textTheme: TextTheme(
-          title: TextStyle(color: Colors.white),
-          display1: TextStyle(color: Colors.white),
-          display2: TextStyle(color: Colors.white),
-          display3: TextStyle(color: Colors.white),
-          display4: TextStyle(color: Colors.white),
-          caption: TextStyle(color: Colors.white),
-          body1: TextStyle(color: Colors.white),
-          body2: TextStyle(color: Colors.white),
-          subtitle: TextStyle(color: Colors.white),
-          subhead: TextStyle(color: Colors.white),
-          button: TextStyle(color: Colors.white),
-          headline: TextStyle(color: Colors.white),
-        ),
-        bottomAppBarColor: Colors.grey[900],
-        dialogTheme: DialogTheme(
-          backgroundColor: Colors.grey[900],
-        ),
-        bottomSheetTheme: BottomSheetThemeData(
-            modalBackgroundColor: Colors.grey[900]
-        ),
-        dividerColor: Colors.grey,
-        inputDecorationTheme: InputDecorationTheme(
-          labelStyle: TextStyle(color: Colors.white),
-          fillColor: Colors.black,
-          hintStyle: TextStyle(color: Colors.grey),
-        ),
-        buttonTheme: ButtonThemeData(
-            textTheme: ButtonTextTheme.accent
-        ),
+      primaryColor: Colors.blueAccent,
+      primarySwatch: Colors.blue,
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+          foregroundColor: Colors.white
+      ),
+      appBarTheme: AppBarTheme(
+        brightness: Brightness.dark,
+        iconTheme: IconThemeData(color: Colors.white70),
+        elevation: 0,
+        color: Colors.grey[900],
+      ),
+      iconTheme: IconThemeData(
+          color: Colors.grey[200]
+      ),
+      scaffoldBackgroundColor: Colors.grey[900],
+      cardColor: Colors.black,
+      indicatorColor: Colors.blueAccent,
+      cursorColor: Colors.blueAccent,
+      accentColor: Colors.grey,
+      textTheme: TextTheme(
+        title: TextStyle(color: Colors.white),
+        display1: TextStyle(color: Colors.white),
+        display2: TextStyle(color: Colors.white),
+        display3: TextStyle(color: Colors.white),
+        display4: TextStyle(color: Colors.white),
+        caption: TextStyle(color: Colors.white),
+        body1: TextStyle(color: Colors.white),
+        body2: TextStyle(color: Colors.white),
+        subtitle: TextStyle(color: Colors.white),
+        subhead: TextStyle(color: Colors.white),
+        button: TextStyle(color: Colors.white),
+        headline: TextStyle(color: Colors.white),
+      ),
+      bottomAppBarColor: Colors.grey[900],
+      dialogTheme: DialogTheme(
+        backgroundColor: Colors.grey[900],
+      ),
+      bottomSheetTheme: BottomSheetThemeData(
+          modalBackgroundColor: Colors.grey[900]
+      ),
+      dividerColor: Colors.grey,
+      inputDecorationTheme: InputDecorationTheme(
+        labelStyle: TextStyle(color: Colors.white),
+        fillColor: Colors.black,
+        hintStyle: TextStyle(color: Colors.grey),
+      ),
+      buttonTheme: ButtonThemeData(
+          textTheme: ButtonTextTheme.accent
+      ),
       chipTheme: ChipThemeData(
         backgroundColor: Colors.grey[700],
         brightness: Brightness.dark,
@@ -304,7 +304,8 @@ class AllpassTheme {
         secondarySelectedColor: Colors.blue,
         selectedColor: Colors.blue,
         shape: StadiumBorder(),
-      )
+      ),
+      canvasColor: Colors.black
     );
   }
 }
