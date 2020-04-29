009b3504976089cc2a4f4c37341dc25888b667e4
Aengus Sun <sunyongsheng6511@gmail.com>
2020-04-29 17:19:34 +0800

    feat(lib/params): 新增 SharedPreferencesKeys保存sp的key

--- lib/params/param.dart
+++ lib/params/param.dart
@@ -1,2 +1,15 @@
 /// 服务器地址
 const String allpassUrl = "http://47.102.208.175:8081/allpass/api";
+
+class SharedPreferencesKeys {
+  static const String firstRun = "FIRST_RUN";
+  static const String needRegister = "NEED_REGISTER";
+  static const String username = "username";
+  static const String password = "password";
+  static const String biometrics = "biometrics";
+  static const String longPressCopy = "longPressCopy";
+  static const String theme = "theme";
+  static const String folder = "folder";
+  static const String label = "label";
+  static const String latestUsePassword = "latestUsePassword";
+}
