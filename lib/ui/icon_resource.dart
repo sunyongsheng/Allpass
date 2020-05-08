b5db72e98921d42fe68d625c992bbb5d13d6fdeb
Aengus Sun <sunyongsheng6511@gmail.com>
2020-05-08 18:14:30 +0800

    feat(lib/ui): 新增 无数据 图标

--- lib/ui/icon_resource.dart
+++ lib/ui/icon_resource.dart
@@ -3,5 +3,8 @@ import 'package:flutter/material.dart';
 /// 自定义图标
 class CustomIcons {
   static const IconData chrome =
-  const IconData(0xe684, fontFamily: "IconFont", matchTextDirection: true);
+      const IconData(0xe7d6, fontFamily: "IconFont", matchTextDirection: true);
+
+  static const IconData noData =
+      const IconData(0xe708, fontFamily: "IconFont", matchTextDirection: true);
 }
