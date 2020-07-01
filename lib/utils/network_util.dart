c1109925fa8e9c6b18b632146e05dcc73d6e91dc
Aengus Sun <sys6511@126.com>
2020-07-01 12:01:55 +0800

    fix(lib/utils): 修复NetworkUtils中函数返回结果类型不是Map的问题

--- lib/utils/network_util.dart
+++ lib/utils/network_util.dart
@@ -10,28 +10,44 @@ class NetworkUtil {
   }
 
   /// 发送注册
-  Future<Map> registerUser(Map<String, String> user) async {
-    Response<Map> response = await _dio.post("$allpassUrl/user/", data: user);
-    return response.data;
+  Future<Map<String, String>> registerUser(Map<String, String> user) async {
+    Response response = await _dio.post<Map<String, String>>("$allpassUrl/user/", data: user);
+    Map<String, String> res = Map();
+    for (String key in response.data.keys) {
+      res[key] = response.data[key];
+    }
+    return res;
   }
 
   /// 发送反馈
-  Future<Map> sendFeedback(Map<String, String> content) async {
-    Response<Map> response = await _dio.post("$allpassUrl/feedback/", data: content);
-    return response.data;
+  Future<Map<String, String>> sendFeedback(Map<String, String> content) async {
+    Response response = await _dio.post("$allpassUrl/feedback/", data: content);
+    Map<String, String> res = Map();
+    for (String key in response.data.keys) {
+      res[key] = response.data[key];
+    }
+    return res;
   }
 
   /// 检查更新
-  Future<Map> checkUpdate() async {
-    Response<Map> response =  await _dio.get(
+  Future<Map<String, String>> checkUpdate() async {
+    Response response =  await _dio.get(
         "$allpassUrl/update?version=${Application.version}");
-    return response.data;
+    Map<String, String> res = Map();
+    for (String key in response.data.keys) {
+      res[key] = response.data[key];
+    }
+    return res;
   }
 
   /// 获取最新版本
-  Future<Map> getLatestVersion() async {
+  Future<Map<String, String>> getLatestVersion() async {
     Response<Map> response = await Dio(BaseOptions(connectTimeout: 10)).get(
         "$allpassUrl/update?version=1.0.0");
-    return response.data;
+    Map<String, String> res = Map();
+    for (String key in response.data.keys) {
+      res[key] = response.data[key];
+    }
+    return res;
   }
 }
