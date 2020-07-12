a8662ca0011d34cc96e6c03676978fd85c10d7a4
Aengus Sun <sys6511@126.com>
2020-07-12 14:27:26 +0800

    fix(lib/pages/setting): 修复推荐给好友的下载地址错误的问题

--- lib/pages/setting/setting_page.dart
+++ lib/pages/setting/setting_page.dart
@@ -407,11 +407,11 @@ class _SettingPage extends State<SettingPage> with AutomaticKeepAliveClientMixin
             subject: "软件推荐——Allpass");
       } else {
         Share.share(
-            "【Allpass】我发现了一款应用，快来下载吧！下载地址：https://allpass.aengus.top/api/download?version=${Application.version}/");
+            "【Allpass】我发现了一款应用，快来下载吧！下载地址：https://allpass.aengus.top/api/download/?version=${Application.version}");
       }
     } catch (e) {
       Share.share(
-        "【Allpass】我发现了一款应用，快来下载吧！下载地址：https://allpass.aengus.top/api/download?version=${Application.version}/");
+        "【Allpass】我发现了一款应用，快来下载吧！下载地址：https://allpass.aengus.top/api/download/?version=${Application.version}");
     }
   }
 }
