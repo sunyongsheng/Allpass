f5948ef220a7d9933a0b94dbc96e663a7efa3286
Aengus Sun <sys6511@126.com>
2020-11-07 00:49:28 +0800

    feat(all): 新增加密等级相关方法

--- lib/core/param/allpass_type.dart
+++ lib/core/param/allpass_type.dart
@@ -35,7 +35,38 @@ class Category {
 
 /// 加密等級
 enum EncryptLevel {
-  All,
+  None,
   OnlyPassword,
-  None
+  All,
+  Unknown
+}
+
+class Encrypt {
+
+  static String getEncryptLevelName(EncryptLevel level) {
+    switch (level) {
+      case EncryptLevel.All:
+        return "全部加密";
+      case EncryptLevel.OnlyPassword:
+        return "仅加密密码字段";
+      case EncryptLevel.None:
+        return "不加密";
+      default:
+        return "未知";
+    }
+  }
+
+  static EncryptLevel getEncryptLevel(String levelName) {
+    switch (levelName) {
+      case "不加密":
+        return EncryptLevel.None;
+      case "全部加密":
+        return EncryptLevel.All;
+      case "仅加密密码字段":
+        return EncryptLevel.OnlyPassword;
+      default:
+        return EncryptLevel.Unknown;
+    }
+  }
+
 }
