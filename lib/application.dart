cb386f6b1267bba2fed71e35f6051926bbd500e3
Aengus Sun <sys6511@126.com>
2020-12-24 15:47:28 +0800

    feat(all): Allpass版本更新1.5.2 -> 1.5.5

--- lib/application.dart
+++ lib/application.dart
@@ -29,7 +29,7 @@ class Application {
   static SharedPreferences sp;
   static MethodChannel shareMethodChannel;
 
-  static String version = "1.5.2";
+  static String version = "1.5.5";
 
   static Future<Null> initSp() async {
     sp = await SharedPreferences.getInstance();
