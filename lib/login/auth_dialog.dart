6d1475a13f79e94f2c482136f709adcc159fc75d
Aengus Sun <sys6511@126.com>
2021-01-05 17:16:01 +0800

    feat(login): 新增 指纹登录对话框(WIP)
    目前插件无法对识别失败的情况提供回调，所以对话框样式不会因为识别失败而变化，用户体验不好，暂时搁浅

---
+++ lib/login/auth_dialog.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:allpass/application.dart';
+import 'package:allpass/core/service/auth_service.dart';
+import 'package:allpass/util/screen_util.dart';
+
+class AuthDialog extends StatefulWidget {
+
+  @override
+  State createState() => _AuthDialogState();
+}
+
+class _AuthDialogState extends State<AuthDialog> {
+
+  final AuthService _localAuthService = Application.getIt<AuthService>();
+
+  bool error = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      if (await _localAuthService.authenticate()) {
+        setState(() {
+          error = false;
+        });
+        Navigator.pop<bool>(context, true);
+      } else {
+        setState(() {
+          error = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _localAuthService.stopAuthenticate();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+      contentPadding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+      children: [
+        Column(
+          children: [
+            Icon(Icons.fingerprint,
+              size: AllpassScreenUtil.setWidth(200),
+              color: error ? Colors.redAccent : null,
+            ),
+            Padding(padding: EdgeInsets.only(top: AllpassScreenUtil.setHeight(20))),
+            Text(error ? "验证失败，请重试" : "请验证指纹",
+              style: error ? TextStyle(color: Colors.redAccent) : null,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
