8cbe8c2aaa7f5268fa03507670f27a51b5103a52
Aengus Sun <sys6511@126.com>
2022-12-31 02:24:15 +0800

    feat(all): Toast支持传入空字符串

--- lib/util/toast_util.dart
+++ lib/util/toast_util.dart
@@ -6,17 +6,25 @@ class ToastUtil {
 
   ToastUtil._();
 
-  static void show({required String msg}) {
-    Fluttertoast.showToast(msg: msg,
-        gravity: ToastGravity.CENTER,
-        backgroundColor: Colors.black87,
-        textColor: Colors.white);
+  static void show({required String? msg}) {
+    if (msg != null && msg.isNotEmpty) {
+      Fluttertoast.showToast(
+          msg: msg,
+          gravity: ToastGravity.CENTER,
+          backgroundColor: Colors.black87,
+          textColor: Colors.white
+      );
+    }
   }
 
-  static void showError({required String msg}) {
-    Fluttertoast.showToast(msg: msg,
-        gravity: ToastGravity.CENTER,
-        backgroundColor: Color.fromRGBO(255, 0, 76, 0.87),
-        textColor: Colors.white);
+  static void showError({required String? msg}) {
+    if (msg != null && msg.isNotEmpty) {
+      Fluttertoast.showToast(
+          msg: msg,
+          gravity: ToastGravity.CENTER,
+          backgroundColor: Color.fromRGBO(255, 0, 76, 0.87),
+          textColor: Colors.white
+      );
+    }
   }
 }
