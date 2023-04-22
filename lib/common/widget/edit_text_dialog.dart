31e1c35370b5dff2495c3305059fce088a61c6dd
Aengus Sun <sys6511@126.com>
2023-04-22 19:10:33 +0800

    feat(all): 支持显示提示文本及校验

--- lib/common/widget/edit_text_dialog.dart
+++ lib/common/widget/edit_text_dialog.dart
@@ -1,40 +1,57 @@
 import 'package:allpass/common/widget/none_border_circular_textfield.dart';
 import 'package:flutter/material.dart';
 
-class EditTextDialog extends StatelessWidget {
+class EditTextDialog extends StatefulWidget {
   final String dialogTitle;
   final String initialText;
+  final String? helpText;
+  final bool Function(String)? validator;
   final ValueSetter<String>? onConfirm;
   final VoidCallback? onCancel;
 
+  EditTextDialog({
+    Key? key,
+    required this.dialogTitle,
+    required this.initialText,
+    this.onConfirm,
+    this.onCancel,
+    this.helpText,
+    this.validator,
+  }) : super(key: key);
+
+  @override
+  State<EditTextDialog> createState() {
+    return _EditTextDialogState();
+  }
+}
+
+class _EditTextDialogState extends State<EditTextDialog> {
+
   final TextEditingController _editingController = TextEditingController();
 
-  EditTextDialog(
-      {Key? key,
-      required this.dialogTitle,
-      required this.initialText,
-      this.onConfirm,
-      this.onCancel})
-      : super(key: key) {
-    _editingController.text = initialText;
+  bool _confirmEnable = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _editingController.text = widget.initialText;
+    _confirmEnable = widget.validator?.call(widget.initialText) != false;
   }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text(dialogTitle),
-      content: NoneBorderCircularTextField(
-        needPadding: false,
-        editingController: _editingController,
-        autoFocus: true,
-        obscureText: false,
-        onEditingComplete: () => _submit(context, _editingController.text),
+      title: Text(widget.dialogTitle),
+      content: SingleChildScrollView(
+        child: Column(children: _content(context)),
       ),
       actions: <Widget>[
         TextButton(
           child: Text(
             "确认",
-            style: TextStyle(color: Theme.of(context).primaryColor),
+            style: _confirmEnable
+                ? TextStyle(color: Theme.of(context).primaryColor)
+                : TextStyle(color: Colors.grey),
           ),
           onPressed: () => _submit(context, _editingController.text),
         ),
@@ -42,15 +59,49 @@ class EditTextDialog extends StatelessWidget {
           child: Text("取消", style: TextStyle(color: Colors.grey)),
           onPressed: () {
             Navigator.pop(context);
-            onCancel?.call();
+            widget.onCancel?.call();
           },
         )
       ],
     );
   }
 
+  List<Widget> _content(BuildContext context) {
+    List<Widget> widgetList = [];
+    if (widget.helpText != null) {
+      widgetList.add(Padding(
+        padding: EdgeInsets.only(bottom: 8),
+        child: Text(
+          widget.helpText!,
+          style: Theme.of(context).textTheme.bodySmall?.copyWith(fontSize: 12),
+        ),
+      ));
+    }
+    widgetList.add(NoneBorderCircularTextField(
+      needPadding: false,
+      editingController: _editingController,
+      autoFocus: true,
+      obscureText: false,
+      onEditingComplete: () => _submit(context, _editingController.text),
+      onChanged: (text) {
+        if (widget.validator == null) {
+          return;
+        }
+
+        setState(() {
+          _confirmEnable = widget.validator!.call(text);
+        });
+      },
+    ));
+    return widgetList;
+  }
+
   void _submit(BuildContext context, String text) {
+    if (widget.validator?.call(text) == false) {
+      return;
+    }
+
     Navigator.pop<String>(context, text);
-    onConfirm?.call(text);
+    widget.onConfirm?.call(text);
   }
 }
