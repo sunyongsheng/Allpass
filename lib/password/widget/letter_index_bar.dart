d32662d63d197ebc90fc3617f44ce02a1f30059f
Aengus Sun <sys6511@126.com>
2023-04-27 01:41:09 +0800

    feat(all): 优化指示条样式及性能

--- lib/password/widget/letter_index_bar.dart
+++ lib/password/widget/letter_index_bar.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:allpass/common/ui/allpass_ui.dart';
@@ -22,9 +25,13 @@ class _LetterIndexBar extends State<LetterIndexBar> {
   final List<String> letters = ['#','A','B','C','D','E','F','G',
     'H','I','J','K','L','M','N', 'O','P','Q','R','S','T',
     'U','V','W','X','Y','Z'];
+  final double letterHeight = 16;
 
-  double _tapPositionY = 0;
-  bool _pressed = false;
+  int _tapPosition = 0;
+  double _opacity = 0.0;
+  bool _isHidden = true;
+  Timer? _hideTimer;
+
   late ScrollController _controller;
 
   @override
@@ -35,91 +42,105 @@ class _LetterIndexBar extends State<LetterIndexBar> {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> _children = [];
-    Map<String, int> map = context.watch<PasswordProvider>().letterCountIndex;
-    var keys = map.keys;
-    letters.forEach((item) {
-      _children.add(SizedBox(
-        child: Text(item, style: TextStyle(
-            color: keys.contains(item) ? null : Colors.grey
-        ),),
-        height: 16,
-      ));
-    });
     return Stack(
       children: <Widget>[
         Align(
           alignment: FractionalOffset(1.0, 0),
           child: SizedBox(
             width: 25,
-            height: 432,
+            height: letterHeight * letters.length,
             child: GestureDetector(
               child: SingleChildScrollView(
-                child: Column(
-                  children: _children,
+                child: Selector<PasswordProvider, Map<String, int>>(
+                  selector: (_, provider) => provider.letterCountIndex,
+                  builder: (_, letterCountIndex, __) {
+                    List<Widget> children = [];
+                    var keys = letterCountIndex.keys;
+                    letters.forEach((item) {
+                      children.add(SizedBox(
+                        child: Text(
+                          item,
+                          style: TextStyle(
+                            color: keys.contains(item) ? null : Colors.grey,
+                          ),
+                        ),
+                        height: letterHeight,
+                      ));
+                    });
+                    return Column(
+                      children: children,
+                    );
+                  },
                 ),
               ),
               onTapDown: (detail) {
+                _hideTimer?.cancel();
                 setState(() {
-                  _tapPositionY = detail.localPosition.dy;
-                  _pressed = true;
+                  _tapPosition = _calculateCurrentPosition(detail.localPosition.dy);
+                  _isHidden = false;
+                  _opacity = 1;
                 });
-                Future.delayed(Duration(milliseconds: 800)).then((_) {
+                _hideTimer = Timer(Duration(milliseconds: 800), () {
                   setState(() {
-                    _pressed = false;
+                    _opacity = 0;
                   });
                 });
               },
               onTapUp: (details) {
                 setState(() {
-                  _tapPositionY = details.localPosition.dy;
+                  _tapPosition = _calculateCurrentPosition(details.localPosition.dy);
+                  if (_hideTimer == null) {
+                    _opacity = 0;
+                  }
                 });
-                int index = (_tapPositionY / 16).floor();
-                if (index >= 26) index = 26;
-                if (keys.contains(letters[index])) {
-                  double offset = (map[letters[index]] ?? 0) * 72.0;
-                  if (offset == 0) offset = 0.5;
-                  _controller.animateTo(offset, curve: Curves.easeOut, duration: Duration(milliseconds: 200));
-                }
+                _scrollToTargetPosition(_tapPosition);
               },
               onVerticalDragUpdate: (details) {
+                _hideTimer?.cancel();
+                _hideTimer = null;
                 setState(() {
-                  _pressed = true;
-                  _tapPositionY = details.localPosition.dy;
+                  _opacity = 1;
+                  _isHidden = false;
+                  _tapPosition = _calculateCurrentPosition(details.localPosition.dy);
                 });
               },
               onVerticalDragEnd: (_) {
                 setState(() {
-                  _pressed = false;
+                  _opacity = 0;
                 });
-                int index = (_tapPositionY / 16).floor();
-                if (index >= 26) index = 26;
-                if (keys.contains(letters[index])) {
-                  double offset = (map[letters[index]] ?? 0) * 72.0;
-                  if (offset == 0) offset = 0.5;
-                  _controller.animateTo(offset, curve: Curves.easeOut, duration: Duration(milliseconds: 200));
-                }
+                _scrollToTargetPosition(_tapPosition);
               },
             ),
           ),
         ),
         Offstage(
-          offstage: !_pressed,
-          child: Center(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: AllpassUI.smallBorderRadius,
-                color: context.watch<ThemeProvider>().offstageColor,
-              ),
-              width: 60,
-              height: 60,
-              child: Center(
-                child: Text(letters[(_tapPositionY/16).abs().floor() >= 26
-                    ? 26
-                    : (_tapPositionY/16).abs().floor()],
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.white,),
+          offstage: _isHidden,
+          child: AnimatedOpacity(
+            opacity: _opacity,
+            duration: Duration(milliseconds: 150),
+            onEnd: () {
+              if (_opacity == 0) {
+                setState(() {
+                  _isHidden = true;
+                });
+              }
+            },
+            child: Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: AllpassUI.smallBorderRadius,
+                  color: context.watch<ThemeProvider>().offstageColor,
+                ),
+                width: 60,
+                height: 60,
+                child: Center(
+                  child: Text(
+                    letters[_tapPosition],
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -127,5 +148,19 @@ class _LetterIndexBar extends State<LetterIndexBar> {
         ),
       ],
     );
+  }
+
+  int _calculateCurrentPosition(double dy) {
+    return max(0, min(26, (dy / letterHeight).floor()));
+  }
+
+  void _scrollToTargetPosition(int target) {
+    var map = context.read<PasswordProvider>().letterCountIndex;
+    var keys = map.keys;
+    if (keys.contains(letters[target])) {
+      double offset = (map[letters[target]] ?? 0) * 72.0;
+      if (offset == 0) offset = 0.5;
+      _controller.animateTo(offset, curve: Curves.easeOut, duration: Duration(milliseconds: 200));
+    }
   }
 }
